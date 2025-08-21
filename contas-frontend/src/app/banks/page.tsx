9468114f6913e3814b0c04a7cc24a68ed4d8e249
DPelizoni <dil_neto@yahoo.com.br>
2025-08-21 06:33:29 -0300

    Frontend: Add tabela Banks

--- contas-frontend/src/app/banks/page.tsx
+++ contas-frontend/src/app/banks/page.tsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 import { bankService } from "@/shared/services";
 import BankItem from "@/shared/components/ui/BankItem";
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup, Table } from "react-bootstrap";
+
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 interface Bank {
   id: string;
@@ -37,28 +41,53 @@ export default function BankListPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl mb-4">Bancos</h1>
-      <Link
-        href="/banks/create"
-        className="bg-green-500 px-4 py-2 rounded"
-      >
-        <Button variant="primary">Cadastrar Banco</Button>
+
+      <Link href="/banks/create">
+        <Button variant="primary">
+          <AddBoxOutlinedIcon /> Cadastrar Banco
+        </Button>
       </Link>
+      <hr />
 
-      <ul className="mt-4 space-y-2">
-        {banks.map((bank) => (
-          <li key={bank.id} className="border p-2 flex justify-between">
-            <span>
-              {bank.name} ({bank.code})
-            </span>
-            <div className="flex gap-2">
-              <Link href={`/banks/${bank.id}`}>Detalhes</Link>
-              <Link href={`/banks/${bank.id}/edit`}>Editar</Link>
+      {/* Tabela */}
+      <Table striped hover size="sm">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Código</th>
+            <th>Nome</th>
+            <th className="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {banks.map((bank) => (
+            <tr key={bank.code}>
+              <td>{bank.id}</td>
+              <td>{bank.code}</td>
+              <td>{bank.name}</td>
 
-              <BankItem bankId={bank.id} onRemoved={handleRemoved} />
-            </div>
-          </li>
-        ))}
-      </ul>
+              <td className="d-flex justify-content-center">
+                <ButtonGroup className="d-flex gap-2 col-2" size="sm">
+                  <Link
+                    href={`/banks/${bank.id}`}
+                    className="btn btn-secondary btn-sm"
+                  >
+                    <VisibilityOutlinedIcon fontSize="small" />
+                  </Link>
+
+                  <Link
+                    href={`/banks/${bank.id}/edit`}
+                    className="btn btn-primary btn-sm"
+                  >
+                    <EditOutlinedIcon fontSize="small" />
+                  </Link>
+                  <BankItem bankId={bank.id} onRemoved={handleRemoved} />
+                </ButtonGroup>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
--- contas-frontend/src/shared/components/ui/ButtonDelete.tsx
+++ contas-frontend/src/shared/components/ui/ButtonDelete.tsx
@@ -1,16 +1,15 @@
 "use client";
 
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+
 interface ButtonDeleteProps {
   onDelete: () => void;
 }
 
 export default function ButtonDelete({ onDelete }: ButtonDeleteProps) {
   return (
-    <button
-      onClick={onDelete}
-      className="bg-red-500 text-white px-2 py-1 rounded"
-    >
-      Excluir
+    <button onClick={onDelete} className="btn btn-danger btn-sm">
+      <DeleteOutlineOutlinedIcon />
     </button>
   );
 }
